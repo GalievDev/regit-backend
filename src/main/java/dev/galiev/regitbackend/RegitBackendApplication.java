487794ce7b493ff9028aba4a1647f4903152c674
Vazir Vadirov <ggvalion@gmail.com>
2024-09-27 20:09:46 +0300

    Initial commit

---
+++ src/main/java/dev/galiev/regitbackend/RegitBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.galiev.regitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegitBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegitBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/galiev/regitbackend/RegitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.galiev.regitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegitBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
