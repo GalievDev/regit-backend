36d9b6ae34165ff516b7ce4e9feb1540af8d1ec2
Vazir Vadirov <ggvalion@gmail.com>
2024-09-28 15:32:05 +0300

    Project and user repository

---
+++ src/main/java/dev/galiev/regitbackend/repository/ProjectRepository.java
@@ -0,0 +1,9 @@
+package dev.galiev.regitbackend.repository;
+
+import dev.galiev.regitbackend.model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+}
---
+++ src/main/java/dev/galiev/regitbackend/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package dev.galiev.regitbackend.repository;
+
+import dev.galiev.regitbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
